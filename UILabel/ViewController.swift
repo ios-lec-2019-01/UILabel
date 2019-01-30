d601f94608731c3099961d76023e197592efb104
Jonghyun Kim <jhkim@dit.ac.kr>
2019-01-30 13:43:22 +0900

    Initial Commit

---
+++ UILabel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UILabel
+//
+//  Created by 김종현 on 30/01/2019.
+//  Copyright © 2019 김종현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
